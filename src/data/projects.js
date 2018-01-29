67eb05eb49a2d6c6c961ddaedc6e5fd37457e062
Christiaan Quyn <christianq010@gmail.com>
2018-01-29 19:59:00 +0530

    refactor: Remove unused work content and images

--- src/data/projects.js
+++ src/data/projects.js
@@ -40,48 +40,6 @@ const projects = [
       background: '#558B2F',
     },
   },
-  {
-    className: 'lifeWithBird',
-    description: 'A minimal and clean e-commerce website built for the contemporary clothing brand based in Melbourne.',
-    clientDescription: 'Life with Bird was established in 2002 with a desire to create high quality, beautifully crafted contemporary clothing. The company continues to expand its global following.',
-    links: {
-      website: 'https://lifewithbird.com',
-    },
-    slug: 'life-with-bird',
-    title: 'Life with Bird',
-    cover: require('./life-with-bird.jpg'),
-    theme: {
-      background: '#f4f5fa',
-      light: true, // Invert text color.
-    },
-  },
-  {
-    className: 'ahm',
-    description: 'A complete overhaul of the Australian health insurer\'s website to provide an enhanced user experience.',
-    clientDescription: 'Ahm (Australian Health Management) is a subsidiary of one of Australia’s largest health insurers. The company now handles health cover for over 370,000 people.',
-    links: {
-      website: 'https://ahm.com.au',
-    },
-    slug: 'ahm',
-    title: 'Ahm',
-    cover: require('./ahm.png'),
-    theme: {
-      background: '#000',
-    },
-  },
-  {
-    className: 'mrRed',
-    description: 'An iOS game built using the Unity 3D game engine. A personal project to challenge and inspire oneself.',
-    links: {
-      ios: 'https://itunes.apple.com/app/id1165442603',
-    },
-    slug: 'mr-red',
-    title: 'Mr. Red',
-    cover: require('./mr-red.png'),
-    theme: {
-      background: '#5ab6ff',
-    },
-  },
 ];
 
 export default projects;
